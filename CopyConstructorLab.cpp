c251d79bb3c39e54d1961574651e99199c455d62
Insydian <cr.larue03@gmail.com>
2023-02-09 13:38:46 -0600

    Modified the charVector for the lab Modified in accordance with step 4

--- CopyConstructorLab.cpp
+++ CopyConstructorLab.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MyVector.h"
 #include "MyArray.h"
+using namespace std;
 int main (int argc, char *argv[])
 {
    MyArray<int, 15> intArray;
@@ -12,13 +13,32 @@ int main (int argc, char *argv[])
   MyArray<int,15> copyOfIntArray = intArray;
   for(int i = 0; i < intArray.getElementCount(); ++i)
   {
-     intArray.modifyElement(i, copyOfIntArray.getElementAt(i) * 2);
+     intArray.modifyElement(i, intArray.getElementAt(i) * 2);
 
   }
+  cout << "intArray Modified: ";
   intArray.printArray();
+  cout << "copyOfIntArray: ";
   copyOfIntArray.printArray();
 
+
+  MyVector<char> charVector;
+  for(int i = 0; i < 26; i++)
+  {
+      charVector.pushBack(97 + i);
+  }
+  MyVector<char> copyOfCharVector(charVector);
+  charVector.print();
+  copyOfCharVector.print();
+  for(int i = 0; i < charVector.getSize(); ++i)
+  {
+        charVector[i] = charVector.getElementAt(i)-32;
+  }
+  cout << "CharVector has been modified" << endl;
+  charVector.print();
+  copyOfCharVector.print();
     return 0;
+
 }
  
 
